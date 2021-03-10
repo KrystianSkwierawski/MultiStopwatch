e942b5ffb400c94f51ab765505b547af790e012f
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-03-10 11:25:19 +0100

    Fixed changing favorite projects order when is mobile device

--- src/WebUI/ClientApp/src/app/components/side-menu/side-menu.component.ts
+++ src/WebUI/ClientApp/src/app/components/side-menu/side-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-menu',
@@ -8,26 +8,53 @@ import { Component, ElementRef, OnInit } from '@angular/core';
     '(window:resize)': 'onResize($event)'
   }
 })
-export class SideMenuComponent implements OnInit {
+export class SideMenuComponent implements OnInit, AfterViewInit {
 
   menuIsActivated: boolean = true;
 
   constructor(public elementRef: ElementRef) { }
 
+  ngAfterViewInit(): void {
+    this.toggleContent_disabledScrolling();
+  }
+
   ngOnInit(): void {
-    
+
   }
 
 
   handleToggler(): void {
     this.menuIsActivated = !this.menuIsActivated;
+
+    this.toggleContent_disabledScrolling();
   }
 
-  onResize(event): void {
-    const isDesktopDevice: boolean = (event.target.innerWidth > 768) ? true : false;
-
-    if (isDesktopDevice) {
+  onResize(): void {
+    if (this.isDesktopDevice()) {
       this.menuIsActivated = true;
+    }
+
+    this.toggleContent_disabledScrolling();
+  }
+
+  isDesktopDevice() {
+    return (window.innerWidth > 768) ? true : false;;
+  }
+
+  toggleContent_disabledScrolling() {
+    const element: HTMLEmbedElement = document.querySelector(".content");
+    const className: string = "content_disabled-scrolling";
+
+    // disable scroll if menu is hidden or is desktop device
+    if (element.classList.contains(className) && (this.isDesktopDevice() || (!this.isDesktopDevice() && !this.menuIsActivated))) {
+
+      element.classList.remove(className);
+    }
+    // enable scroll if menu is activated and it is mobile device
+    else if (!element.classList.contains(className) && (!this.isDesktopDevice() && this.menuIsActivated)) {
+      element.classList.add(className);
     }
   }
 }
+
+
