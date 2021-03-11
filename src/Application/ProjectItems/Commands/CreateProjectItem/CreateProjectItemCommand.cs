a313ba18d6261f5bdfc2d393e9ed333b4fc6e8a8
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-03-11 15:13:06 +0100

    Refactored CreateProjectItemCommand

--- src/Application/ProjectItems/Commands/CreateProjectItem/CreateProjectItemCommand.cs
+++ src/Application/ProjectItems/Commands/CreateProjectItem/CreateProjectItemCommand.cs
@@ -23,16 +23,11 @@ namespace Project.Application.ProjectItems.Commands.CreateProjectItem
 
             public async Task<Unit> Handle(CreateProjectItemCommand request, CancellationToken cancellationToken)
             {
-                //int numberOfProjects = _context.ProjectItems.ToList().Count();
-                //int lastIndex = (numberOfProjects > 1) ? numberOfProjects - 1 : numberOfProjects;
-                int lastIndex = _context.ProjectItems.ToList().Count();
-
                 ProjectItem entity = new ProjectItem
                 {
                     Title = request.Title,
                     IsFavorite = false,
                     Time = "00:00",
-                    OrderIndex = lastIndex
                 };
 
                 await _context.ProjectItems.AddAsync(entity);
