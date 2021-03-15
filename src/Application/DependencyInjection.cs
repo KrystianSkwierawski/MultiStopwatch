ea2ce61d03f1e56cf09f8f4d3cc0a08fccf9ecf5
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-03-15 07:17:03 +0100

    Added PerformanceBehaviour and LoggingBehaviour

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -14,6 +14,7 @@ namespace Project.Application
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
 
             return services;
         }
