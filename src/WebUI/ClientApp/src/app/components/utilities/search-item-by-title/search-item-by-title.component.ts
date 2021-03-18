ca46d60ae2335da0045234504bba6d61ecf2940f
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-03-18 14:25:41 +0100

    Fixed refreshing options in SearchItemByTitleComponent

--- src/WebUI/ClientApp/src/app/components/utilities/search-item-by-title/search-item-by-title.component.ts
+++ src/WebUI/ClientApp/src/app/components/utilities/search-item-by-title/search-item-by-title.component.ts
@@ -9,9 +9,6 @@ import { map, startWith } from 'rxjs/operators';
   styleUrls: ['./search-item-by-title.component.scss']
 })
 export class SearchItemByTitleComponent implements OnInit {
-
-  myControl = new FormControl();
-  filteredOptions: Observable<string[]>;
   @Input() options: string[];
   @Output() onKeyUp = new EventEmitter<string>();
   @ViewChild('searchingTitle') searchingTitleInput: ElementRef;
@@ -19,16 +16,7 @@ export class SearchItemByTitleComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value))
-    );
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+ 
   }
 
   keyUp() {
