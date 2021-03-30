3ce414340152a795969045f1e0a348bbafc6f421
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-03-30 16:34:38 +0200

    Added GetStopwatchItemsWithPaginationTests

---
+++ tests/Application.IntegrationTests/StopwatchItems/Queries/GetStopwatchItemsWithPaginationTests.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using Project.Application.Common.Models;
+using Project.Application.StopwatchItems.Queries.GetStopwatchItemsWithPagination;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.IntegrationTests.StopwatchItems.Queries
+{
+    using static Testing;
+
+    public class GetStopwatchItemsWithPaginationTests : TestBase
+    {
+        [Test]
+        public async Task ShouldReturnStopwatchItemsWithPagination()
+        {
+            //Arrange
+            ProjectItem projectItem = new ProjectItem
+            {
+                Title = "Project"
+            };
+            await AddAsync(projectItem);
+
+
+            StopwatchItem stopwatchItem = new StopwatchItem
+            {
+                ProjectItemId = projectItem.Id,
+                Title = "Stopwatch"
+            };
+            await AddAsync(stopwatchItem);
+
+            var query = new GetStopwatchItemsWithPaginationQuery
+            {
+                ProjectId = projectItem.Id,
+                PageNumber = 1,
+                PageSize = 50
+            };
+
+            //Act
+            PaginatedList<StopwatchItemDto> result = await SendAsync(query);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Items.Should().HaveCount(1);
+            result.Items.First().Title.Should().Be(stopwatchItem.Title);
+        }
+    }
+}
