ff95b741ae2bdaf1fec4f9dc2ac83d9e3bdf27af
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-04-05 09:01:09 +0200

    Added LikeOrDislikeProjectItemTests

---
+++ tests/Application.IntegrationTests/SplittedTimes/Commands/LikeOrDislikeProjectItemTests.cs
@@ -0,0 +1,62 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using Project.Application.FavoriteProjectItems.Commands.LikeOrDislikeProjectItem;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.IntegrationTests.SplittedTimes.Commands
+{
+    using static Testing;
+    public class LikeOrDislikeProjectItemTests
+    {
+        [Test]
+        public async Task ShouldLikeProjectItem()
+        {
+            //Arrange
+            ProjectItem projectItem = new ProjectItem
+            {
+                Title = "project",
+                IsFavorite = false
+            };
+            await AddAsync(projectItem);
+
+            var command = new LikeOrDislikeProjectItemCommand
+            {
+                Id = projectItem.Id
+            };
+
+            //Act
+            await SendAsync(command);
+
+            //Assert
+            ProjectItem result = await FindAsync<ProjectItem>(command.Id);
+            result.Should().NotBeNull();
+            result.IsFavorite.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task ShouldDisikeProjectItem()
+        {
+            //Arrange
+            ProjectItem projectItem = new ProjectItem
+            {
+                Title = "project",
+                IsFavorite = true
+            };
+            await AddAsync(projectItem);
+
+            var command = new LikeOrDislikeProjectItemCommand
+            {
+                Id = projectItem.Id
+            };
+
+            //Act
+            await SendAsync(command);
+
+            //Assert
+            ProjectItem result = await FindAsync<ProjectItem>(command.Id);
+            result.Should().NotBeNull();
+            result.IsFavorite.Should().BeFalse();
+        }
+    }
+}
