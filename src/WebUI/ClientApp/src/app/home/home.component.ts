0e6c56ceeae496d05e02462f808c97f28a43c915
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-08-04 13:31:52 +0200

    Fix animations on scrolls
    AOS needs to refresh offsets after init.

--- src/WebUI/ClientApp/src/app/home/home.component.ts
+++ src/WebUI/ClientApp/src/app/home/home.component.ts
@@ -1,33 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { take } from 'rxjs/operators';
 import { AuthenticationService } from '../authentication/authentication.service';
 import { LoginDialogComponent } from '../authentication/login-dialog/login-dialog.component';
 import { RegisterDialogComponent } from '../authentication/register-dialog/register-dialog.component';
-import * as AOS from 'aos';
-
+import AOS from 'aos';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   isAuthenticated: boolean;
+  scrollRef = 0;
 
   constructor(private _dialog: MatDialog,
     private _router: Router,
     private _authService: AuthenticationService
   ) { }
-
+   
   ngOnInit(): void {
-    AOS.init();
-
     this._authService.isAuthenticated.pipe(take(1)).subscribe(isAuthenticated => {
       this.isAuthenticated = isAuthenticated;
     });
+  }
+
+  ngAfterViewInit(): void {
+    AOS.init({ offset: 350 });
   }
 
   openRegisterDialog() {
@@ -50,5 +52,10 @@ export class HomeComponent implements OnInit {
     this._dialog.open(LoginDialogComponent, {
       panelClass: 'login-dialog'
     });
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.scrollRef <= 5 ? this.scrollRef++ : AOS.refresh();
   }
 }
