1a98826747bc6faab50108f7159114ecb21bd559
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-08-17 13:37:52 +0200

    Remove unsused code

--- src/WebUI/ClientApp/src/environments/environment.ts
+++ src/WebUI/ClientApp/src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  urlAddress: 'https://localhost:44319'
+  production: false
 };
 
 /*
