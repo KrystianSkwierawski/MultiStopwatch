8a2214ba7a84298e2993bc35799bed347831998c
Krystian Skwierawski <bezi25251@wp.pl>
2022-05-09 17:50:41 +0200

    Fix routing

--- src/WebUI/ClientApp/src/app/app-routing.module.ts
+++ src/WebUI/ClientApp/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'app', loadChildren: () => import('./application/application.module').then(m => m.ApplicationModule) },
-  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
